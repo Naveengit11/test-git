7174420d10839e7421e5cc2cbbf6ce6ffbf1a4ea
Naveengit11 <naveenmirada@gmail.com>
2021-02-19 07:08:40 +0000

    hook

--- appui/src/trial-component/Trial-component.js
+++ appui/src/trial-component/Trial-component.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Comp = (props) => {
+    const [name] = useState('Naveen2');
     return (
         <div>
             <h1>Temp component</h1>
             <p>User : {props.name}</p>
+            <p> From state : {name} </p>
             <p>This is a temp component</p>
         </div>
     )
