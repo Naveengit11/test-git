b8f3013ca2a13ed8d8f18aa2a2d4d48cb2ccde4b
Naveengit11 <naveenmirada@gmail.com>
2021-02-19 06:24:56 +0000

    test 3

--- appui/src/trial-component/Trial-component.js
+++ appui/src/trial-component/Trial-component.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Comp = () => {
+const Comp = (props) => {
     return (
         <div>
             <h1>Temp component</h1>
+            <p>User : {props.name}</p>
             <p>This is a temp component</p>
         </div>
     )
